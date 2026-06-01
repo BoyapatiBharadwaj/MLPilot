702e42cb7ad8195b7ce98d68bf7703f9eec34e1d
Bharadwaj Boyapati <117799879+BoyapatiBharadwaj@users.noreply.github.com>
2026-06-01 17:57:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ from modules.exporter import (
     export_csv
 )
 
+import streamlit.components.v1 as components
+
 
 st.set_page_config(
     page_title="MLPilot",
@@ -58,15 +60,23 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.html("""
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FNY2CSGBB6"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-FNY2CSGBB6');
-</script>
-""")
+components.html(
+    """
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){
+            (c[a].q=c[a].q||[]).push(arguments)
+        };
+        t=l.createElement(r);
+        t.async=1;
+        t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x082turlkt");
+    </script>
+    """,
+    height=0
+)
 
 st.markdown("""
 <style>
